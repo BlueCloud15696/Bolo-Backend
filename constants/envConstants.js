8506ebbdf91d807f3599a029f43249dee4d8ba76
BlueCloud15696 <webmaster15696@gmail.com>
2022-09-23 06:01:53 -0700

    fixed aws s3 connection issue

--- constants/envConstants.js
+++ constants/envConstants.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const envConstants = {
     USER_VERIFICATION_TOKEN_SECRET: "dgfgpspdifgskdfngussj490385jsp8ms",
     secret: "gew67dfgew", //secret key
@@ -5,10 +6,10 @@ const envConstants = {
     email: "your email",
     emailPass: "email password",
     BASE_URL: "https://localhost:3000",
-    Access_key_ID: "AKIAS2EKNRHCA4WZ4T4P",
-    Secret_access_key: "wMmvdIZi3koSU8P/Va1357M8AQUPTU/gJAp7cixN",
-    bucket_name: "bolo-image-bucket",
-    bucket_region: "us-east-1",
+    Access_key_ID: process.env.Access_key_ID || ""/* "AKIAS2EKNRHCA4WZ4T4P" */,
+    Secret_access_key: process.env.Secret_access_key || ""/* "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" */,
+    bucket_name: process.env.bucket_name || ""/* "bolo-image-bucket" */,
+    bucket_region: process.env.bucket_region || ""/* "us-east-1" */,
     password: "'*y1UR7J3sV1RoU"
 };
 
